1ae03168240b96ede644317558a19a12cda15218
Tyler Chalifoux <82053574+TylerChalifoux@users.noreply.github.com>
2022-09-20 18:03:40 -0400

    finished new account page
    Added a new test location file as well to get the location testing

---
+++ README.txt
@@ -0,0 +1,16 @@
+-----------------FIND A LOOP web app------------------
+
+
+CURRENT BUILD INFORMATION-------------
+- Two web pages: a landing page and a create an account page
+- The landing page is simple. asks for username/ password or allows to make an account
+- If you click on "create an account" you are brought to the create an account page
+- the create an account page asks for a username and two passwords (one to verify)
+- The passwod verifacation works. Password must be 8 or more digits and must match
+
+
+Next build goals------
+- We need to build the home page soon, create the home page
+- One the home page, begin working on getting data from the gps on the phone to google maps API
+- Take that information and put it into the webpage
+- A new HTML file was created (location_test.html) to test the way that location gathering will work
--- new_account.js
+++ new_account.js
@@ -2,18 +2,24 @@ $(document).ready(function(){
     $('#signUpButton').click(function(){
         var topPassword = $('#topPasswordBox').val();
         var bottomPassword = $('#bottomPasswordBox').val();
+        $('.passwordBox').css("border-color", "black");
+        $('#errorMessageTop').css("color", "black");
+        $('#errorMessageBottom').css("color", "black");
+        var goodPassword = true;
         
-        if(topPassword != bottomPassword){
-            $('#passwordBox').css('border')
-            $('#errorMessage').text("ERROR: Passwords do not match");
-        }else{
-            console.log("They dont match");
+        if(topPassword.length<7 || bottomPassword.length<7){
+            $('.passwordBox').css("border-color", "red");
+            $('#errorMessageTop').css("color", "red");
+            goodPassword = false;
         }
-    
-    
-    
+        if(topPassword != bottomPassword){
+            $('.passwordBox').css("border-color", "red");
+            $('#errorMessageBottom').css("color", "red");
+            goodPassword = false;
+        }
+        
+        //ONCE THE PASSWORD HAS BEEN VERIFIED TO BE GOOD
+        if(goodPassword){
+        }
     });
-
-
-    
 });
