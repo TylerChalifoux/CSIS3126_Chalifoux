d0947813acab668caf7046257c79cc0e190aded4
Tyler Chalifoux <82053574+TylerChalifoux@users.noreply.github.com>
2022-09-28 14:25:21 -0400

    Update mobile.js
    Oops! last update was still inside the timer function causing it to get the location every milisecond so we made a new one for every second

--- mobile.js
+++ mobile.js
@@ -54,20 +54,17 @@ $(document).ready(function(){
         $('#timerBox').css("background-color","green");
         setInterval(function() {
             timerInfo();
-
-                                function success(pos) {
-                                    const crd = pos.coords;
-                                
-                                    console.log('Your current position is:');
-                                    console.log(`Latitude : ${crd.latitude}`);
-                                    console.log(`Longitude: ${crd.longitude}`);
-                                    console.log(`More or less ${crd.accuracy} meters.`);
-                                }
-                                
-                                navigator.geolocation.getCurrentPosition(success);
-
-
         }, 1);
+        setInterval(function() {
+        function success(pos) {
+            const crd = pos.coords;
+            console.log('Your current position is:');
+            console.log(`Latitude : ${crd.latitude}`);
+            console.log(`Longitude: ${crd.longitude}`);
+            console.log(`More or less ${crd.accuracy} meters.`);
+        }
+            navigator.geolocation.getCurrentPosition(success);
+         }, 1000);
     });
 
     document.getElementById('stopButton').addEventListener('click', ()=>{
