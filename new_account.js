c9017e0dc118f0655469e208ab2be8f984d660bf
Tyler Chalifoux <82053574+TylerChalifoux@users.noreply.github.com>
2022-10-05 11:18:49 -0400

    Update new_account.js

--- new_account.js
+++ new_account.js
@@ -21,19 +21,20 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
 
-
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-auth.js";
 import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-auth.js";
 
 const auth = getAuth();
 createUserWithEmailAndPassword(auth, email, password)
   .then((userCredential) => {
-    
+    // Signed in 
     const user = userCredential.user;
+    // ...
   })
   .catch((error) => {
     const errorCode = error.code;
     const errorMessage = error.message;
-    
+    // ..
   });
 
 
