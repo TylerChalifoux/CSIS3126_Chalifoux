53e00df86a723fa440b6ef0ea44f101a5c2bd81d
Tyler Chalifoux <82053574+TylerChalifoux@users.noreply.github.com>
2022-10-17 12:05:18 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+window.onload = function(){
+    
+    var http = new XMLHttpRequest();
+    http.open("GET","C:\Users\Owner\OneDrive\Desktop\3126 Project\CSIS3126_Chalifoux\new_account_page.html",true);
+    http.send();
+    console.log(http);
+
+
+
+}
